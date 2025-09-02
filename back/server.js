56363432767f8eb7c91c993d8cd3addecf2f51a2
Rohit Patil <143780223+rohitpatilgithub@users.noreply.github.com>
2025-09-02 07:55:31 +0530

    Deployment

--- back/server.js
+++ back/server.js
@@ -19,9 +19,9 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.static("client/build"));
+app.use(express.static("../frontend/build"));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
 });
 
 app.use('/api/users',userRoutes);
